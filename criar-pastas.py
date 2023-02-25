5c2ee7bad684c5daa599de370dd9958f59405b0e
rafael <rafaelalvesraposo@hotmail.com>
2023-02-25 17:11:23 -0300

    pastas com nomes

--- criar-pastas.py
+++ criar-pastas.py
@@ -1,6 +1,6 @@
 import os 
 
 nomes = ["Danilo","Davi","Eduarda","Kelvin","Rafael","Ryan"] 
-
+os.system("mkdir entregavel-spr1") 
 for i in nomes : 
-    os.system(f"mkdir {i} && touch '.' > {i}/entregavel-diego.txt")
+    os.system(f"mkdir entregavel-spr1/{i} && touch '.' > entregavel-spr1/{i}/entregavel-diego.txt")
