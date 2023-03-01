aa772e3828e10bdc60a070ae7656d6dd495aea5c
eduardanv <maria.neves@sptech.school>
2023-03-01 17:36:30 -0300

    subindo a atv de pi/progweb

---
+++ entregavel-spr1/Eduarda/login-02221000-mariaeduarda/login-02221000-mariaeduarda/login-02221000-mariaeduarda/src/test/java/sptech/school/login02221000mariaeduarda/Login02221000MariaeduardaApplicationTests.java
@@ -0,0 +1,13 @@
+package sptech.school.login02221000mariaeduarda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Login02221000MariaeduardaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
