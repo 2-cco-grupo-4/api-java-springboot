dc605edb34dbdf240d93645c5228f989881d012c
DanyloSPTech <danylo.gomes@sptech.school>
2023-03-05 21:17:34 -0300

    api rest login danylo

--- entregavel-spr1/Danylo/login-02221004-danylo/src/test/java/login02221004danylo/Login02221004DanyloApplicationTests.java
+++ entregavel-spr1/Danylo/login-02221004-danylo/src/test/java/login02221004danylo/Login02221004DanyloApplicationTests.java
@@ -1,10 +1,10 @@
-package sptech.school.login02221000mariaeduarda;
+package login02221004danylo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Login02221000MariaeduardaApplicationTests {
+class Login02221004DanyloApplicationTests {
 
 	@Test
 	void contextLoads() {
