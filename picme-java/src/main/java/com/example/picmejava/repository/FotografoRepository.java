5ee3018b656fef45708f4996a93c8366b8c8fdaf
davibordon <davi.silva@sptech.school>
2023-04-02 17:20:26 -0300

    FEAT(): Add banco H2, FotografoRepository

---
+++ picme-java/src/main/java/com/example/picmejava/repository/FotografoRepository.java
@@ -0,0 +1,7 @@
+package com.example.picmejava.repository;
+
+import com.example.picmejava.model.Fotografo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FotografoRepository extends JpaRepository<Fotografo, Integer> {
+}
