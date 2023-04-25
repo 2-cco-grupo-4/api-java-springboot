4ec02d321281ab29e029c1936b615528e588ed3d
davibordon <davi.silva@sptech.school>
2023-04-25 15:11:21 -0300

    FIX(): Removendo Spring Security, arrumando listar Fotografo

--- picme-java/src/main/java/com/example/picmejava/model/mapper/FotografoMapper.java
+++ picme-java/src/main/java/com/example/picmejava/model/mapper/FotografoMapper.java
@@ -4,20 +4,8 @@ import com.example.picmejava.model.Fotografo;
 import com.example.picmejava.model.dto.AtualizarUsuarioDTO;
 import com.example.picmejava.model.dto.CadastroUsuarioDTO;
 import com.example.picmejava.model.dto.PerfilFotografoDTO;
-import com.example.picmejava.service.autenticacao.dto.UsuarioTokenDTO;
 
 public class FotografoMapper {
-
-    public static UsuarioTokenDTO of(Fotografo fotografo, String token){
-        UsuarioTokenDTO usuarioTokenDTO = new UsuarioTokenDTO();
-
-        usuarioTokenDTO.setUserId(fotografo.getId());
-        usuarioTokenDTO.setNome(fotografo.getNome());
-        usuarioTokenDTO.setEmail(fotografo.getEmail());
-        usuarioTokenDTO.setToken(token);
-
-        return usuarioTokenDTO;
-    }
 
     public PerfilFotografoDTO toPerfilFotogradoDTO(Fotografo fotografo){
         PerfilFotografoDTO dto = new PerfilFotografoDTO();
