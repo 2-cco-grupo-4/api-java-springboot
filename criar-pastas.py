5012a6ef13ed1019b63ed8eea89ccea69adaeee5
davibordon <davi.silva@sptech.school>
2023-05-21 12:39:33 -0300

    FEAT() removendo entregáveis sprint 1 + python

--- criar-pastas.py
+++
@@ -1,6 +0,0 @@
-import os 
-
-nomes = ["Danilo","Davi","Eduarda","Kelvin","Rafael","Ryan"] 
-os.system("mkdir entregavel-spr1") 
-for i in nomes : 
-    os.system(f"mkdir entregavel-spr1/{i} && touch '.' > entregavel-spr1/{i}/entregavel-diego.txt")
