46933c9e2ec95aeec5c0719500b892b39f4f8dbd
davibordon <davi.silva@sptech.school>
2023-06-06 17:44:15 -0300

    FEAT(): adicionando as validações

--- picme-java/src/main/java/com/example/picmejava/service/usuario/dto/AtualizarUsuarioDTO.java
+++ picme-java/src/main/java/com/example/picmejava/service/usuario/dto/AtualizarUsuarioDTO.java
@@ -1,5 +1,8 @@
 package com.example.picmejava.service.usuario.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,10 +11,17 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class AtualizarUsuarioDTO {
-
+    @NotBlank
+    @Size(max = 32, min = 1)
     private String nome;
+    @NotBlank
+    @Size(max = 32, min = 8)
     private String senha;
+    @NotBlank
+    @Size(max = 15, min = 11)
     private String numCelular;
+    @Past
+    @NotBlank
     private LocalDate dataNasc;
 }
 
--- picme-java/src/main/java/com/example/picmejava/service/usuario/dto/CadastroUsuarioDTO.java
+++ picme-java/src/main/java/com/example/picmejava/service/usuario/dto/CadastroUsuarioDTO.java
@@ -1,7 +1,9 @@
 package com.example.picmejava.service.usuario.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 
@@ -9,10 +11,22 @@ import java.time.LocalDate;
 @Setter
 public class CadastroUsuarioDTO {
 
+    @NotBlank
+    @Size(max = 32, min = 1)
     private String nome;
+    @CPF
+    @NotBlank
     private String cpf;
+    @Past
+    @NotBlank
     private LocalDate dataNasc;
+    @Email
+    @NotBlank
     private String email;
+    @NotBlank
+    @Size(max = 32, min = 8)
     private String senha;
+    @NotBlank
+    @Size(max = 15, min = 11)
     private String numCelular;
 }
