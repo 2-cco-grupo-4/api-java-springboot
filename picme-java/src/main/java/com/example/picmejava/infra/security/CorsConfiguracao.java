8b6d9b6cd661f8c4b3b5e78d9d98be6adcd859b7
davi.silva <davi.silva@sptech.school>
2023-09-07 15:44:49 -0300

    FEAT(): alterando ip publico picme-site

--- picme-java/src/main/java/com/example/picmejava/infra/security/CorsConfiguracao.java
+++ picme-java/src/main/java/com/example/picmejava/infra/security/CorsConfiguracao.java
@@ -10,7 +10,7 @@ public class CorsConfiguracao implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://3.236.75.139")
+                .allowedOrigins("http://44.201.32.11")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
