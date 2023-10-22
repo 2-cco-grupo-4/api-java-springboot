c879877d5e5afa6fb78259c65f083061c33e6d44
DanyloSPTech <danylo.gomes@sptech.school>
2023-10-22 15:33:49 -0300

    feat(PIC-141): Suporte a gráfico com duas barras

--- picme-java/src/main/java/com/example/picmejava/service/dashboard/dto/admin/vwTemaCountSessoes.java
+++ picme-java/src/main/java/com/example/picmejava/service/dashboard/dto/admin/vwTemaCountSessoes.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,4 +15,5 @@ public class vwTemaCountSessoes {
 
     private String tema;
     private Long sessoes;
+    private BigDecimal valor;
 }
