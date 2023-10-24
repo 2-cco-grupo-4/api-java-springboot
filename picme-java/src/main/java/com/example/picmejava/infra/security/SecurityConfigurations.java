4a4d26b3ab6f031cb6aa384535da0e1dafeb0889
DanyloSPTech <danylo.gomes@sptech.school>
2023-10-24 01:31:45 -0300

    feat(Arquivos): ajuste acessos fotografo

--- picme-java/src/main/java/com/example/picmejava/infra/security/SecurityConfigurations.java
+++ picme-java/src/main/java/com/example/picmejava/infra/security/SecurityConfigurations.java
@@ -51,8 +51,8 @@ public class SecurityConfigurations implements WebMvcConfigurer {
                     req.requestMatchers(HttpMethod.PUT, "/imagens").hasAnyRole("FOTOGRAFO", "ADMIN");
                     req.requestMatchers(HttpMethod.POST, "/albums").hasAnyRole("FOTOGRAFO", "ADMIN");
                     req.requestMatchers(HttpMethod.POST, "/temas").hasAnyRole("FOTOGRAFO", "ADMIN");
-                    req.requestMatchers(HttpMethod.GET, "/arquivo/**").hasAnyRole("ADMIN");
-                    req.requestMatchers(HttpMethod.POST, "/arquivo/**").hasAnyRole("ADMIN");
+                    req.requestMatchers(HttpMethod.GET, "/arquivo/**").hasAnyRole("ADMIN", "FOTOGRAFO");
+                    req.requestMatchers(HttpMethod.POST, "/arquivo/**").hasRole("ADMIN");
                     req.requestMatchers(HttpMethod.POST, "/clientes/cadastrarExterno").hasRole("FOTOGRAFO");
                     req.requestMatchers("/admin/**").hasRole("ADMIN");
                     req.anyRequest().authenticated();
